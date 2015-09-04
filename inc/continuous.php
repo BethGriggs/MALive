7153fa3f4f6ac2e6ec8fd692ff9cf01bfc2a7186
BethGriggs <bethanygriggs@outlook.com>
2015-09-04 20:04:24 +0100

    Add handsontable, continuous hedges g table

--- inc/continuous.php
+++ inc/continuous.php
@@ -1,1 +1,22 @@
-<h1>Continuous</h1>
+<script src="lib/handsontable-0.17.0/dist/handsontable.full.js"></script>
+<link rel="stylesheet" media="screen" href="lib/handsontable-0.17.0/dist/handsontable.full.css">
+ 
+<h1>Continuous (Hedges g)</h1>
+
+<div id="continuous-hedges-g"></div>
+<script>
+  var data = [
+    ["Study", "N1", "N2", "M1", "M2", "SD1", "SD2", "MeanDiff", "CI-", "CI+"]
+  ];
+
+  var container = document.getElementById('continuous-hedges-g');
+  var meanDiff = new Handsontable(container,
+    {
+      data: data,
+      minSpareRows: 1,
+      colHeaders: ["Study", "N1", "N2", "M1", "M2", "SD1", "SD2", "MeanDiff", "CI-", "CI+"],
+      columnSorting: true,
+      contextMenu: true,
+    });
+   
+</script>
