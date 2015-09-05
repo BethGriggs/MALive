0f251df4943a9a601f9a2db69305ce10b7467e59
BethGriggs <bethanygriggs@outlook.com>
2015-09-05 21:37:39 +0100

    Add other continuous tables (test)

--- inc/continuous.php
+++ inc/continuous.php
@@ -4,17 +4,51 @@
 <h1>Continuous (Hedges g)</h1>
 
 <div id="continuous-hedges-g"></div>
+<h1>Continuous R</h1>
+<div id="continuous-r"></div>
+<h1>Continuous Mean Difference</h1>
+<div id="continuous-mean-difference"></div>
+
 <script>
-  var data = [
-    ["Study", "N1", "N2", "M1", "M2", "SD1", "SD2", "MeanDiff", "CI-", "CI+"]
+  var hedgesdata = [
+    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
   ];
 
-  var container = document.getElementById('continuous-hedges-g');
-  var meanDiff = new Handsontable(container,
+  var hedgesg = document.getElementById('continuous-hedges-g');
+  var meanDiff = new Handsontable(hedgesg,
     {
-      data: data,
+      data: hedgesdata,
       minSpareRows: 1,
-      colHeaders: ["Study", "N1", "N2", "M1", "M2", "SD1", "SD2", "MeanDiff", "CI-", "CI+"],
+      colHeaders: ["Study", "N1", "N2", "M1", "M2", "SD1", "SD2", "Hedges g", "CI-", "CI+"],
+      columnSorting: true,
+      contextMenu: true,
+    });
+   
+      var rdata = [
+    ["1", "1", "1","1","1","1"]
+  ];
+
+  var r = document.getElementById('continuous-r');
+  var meanDiff = new Handsontable(r,
+    {
+      data: rdata,
+      minSpareRows: 1,
+      colHeaders: ["Study", "Size", "Correlation", "CI-", "CI+"],
+      columnSorting: true,
+      contextMenu: true,
+    });
+    
+       
+      var meandiffdata = [
+    ["1", "1", "1","1","1","1","1","1", "1"]
+  ];
+
+  var r = document.getElementById('continuous-mean-difference');
+  var meanDiff = new Handsontable(r,
+    {
+      data: meandiffdata,
+      minSpareRows: 1,
+      colHeaders: ["Study", "N1", "N2", "M1", "SD1", "SD2", "Mean Difference", "CI-", "CI+"],
       columnSorting: true,
       contextMenu: true,
     });
